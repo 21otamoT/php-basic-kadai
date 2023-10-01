465e5e4039f67e8ec4f94f3ff3d7c85a619ec2f5
21otamoT <135295588+21otamoT@users.noreply.github.com>
2023-10-01 20:53:09 +0900

    Update associative-array.php

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -1,5 +1,16 @@
-<?php
-  $onion = ['name'=>'onion','price'=>200,'weight'=>160];
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>PHPの課題７</title>
+  </head>
+  <body>
+    <p>
+      <?php
+       $onion = ['name'=>'onion','price'=>200,'weight'=>160];
 
-  print_r($onion);
-?>
+       print_r($onion);
+      ?>
+    </p>
+  </body>
+</html>
